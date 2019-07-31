4ec88840b577a87dbf508c8ce8a87be957d111ad
Antonio Giliberto <antonio.giliberto@ngpems.com.mt>
2019-07-31 09:55:28 +0200

    fixed 2 ways binding

--- src/pages/DateTimePicker.vue
+++ src/pages/DateTimePicker.vue
@@ -1,99 +1,105 @@
 <template>
-  <div class="timepickerWrapper">
-    <section class="demoSection">
-      <DateTimePicker
-        :singleDate="singleDate"
-        :start-date="startDate"
-        :end-date="endDate"
-        timeFormat="HH:mm"
-        @onChange="dateTimePickerValue = $event"
-      />
-    </section>
+    <div class="timepickerWrapper">
+        <section class="demoSection">
+            <DateTimePicker
+                :end-date="endDate"
+                :singleDate="singleDate"
+                :start-date="startDate"
+                @onChange="dateTimePickerValue = $event"
+                timeFormat="HH:mm"
+            />
+            <button @click="startDate = new Date('2018-01-01 00:00:00')">Last year</button>
+            <button @click="endDate = new Date('2018-12-01 00:00:00')">full year</button>
+        </section>
 
-    <section class="resultSection">
-      <h2>Result</h2>
-      <div class="exampleWrapper">
-        <pre v-highlightjs="resultString"><code class="javascript"></code></pre>
-      </div>
-    </section>
+        <section class="resultSection">
+            <h2>Result</h2>
+            <div class="exampleWrapper">
+                <pre v-highlightjs="resultString"><code class="javascript"></code></pre>
+            </div>
+        </section>
 
-    <section class="sampleSection">
-      <h2>Sample</h2>
-      <div class="exampleWrapper">
-        <pre v-highlightjs="sourcecode"><code class="javascript"></code></pre>
-      </div>
-    </section>
-  </div>
+        <section class="sampleSection">
+            <h2>Sample</h2>
+            <div class="exampleWrapper">
+                <pre v-highlightjs="sourcecode"><code class="javascript"></code></pre>
+            </div>
+        </section>
+    </div>
 </template>
 
 <script>
-import DateTimePicker from "../components/DateTimePicker";
+    import DateTimePicker from "../components/DateTimePicker"
 
 
-export default {
-  name: "time-picker-page",
-  methods: {
-    submitHandler: function(data) {
-      console.log("data: ", data);
-    },
-    cancelHandler: function() {
-      console.log("close");
-    }
-  },
-  components: {
-    DateTimePicker
-  },
-  computed: {
-    resultString: function() {
-      return JSON.stringify(this.dateTimePickerValue, null, 4);
-    }
-  },
-  data: function() {
-    return {
-      startDate: new Date(),
-      endDate: new Date(),
-      singleDate: false,
-      dateTimePickerValue: "",
-      sourcecode: `
-<TimePicker v-model="timePickerValue" format="hh:mm:A" />
+    export default {
+        name: "time-picker-page",
+        methods: {
+            submitHandler: function (data) {
+                console.log("data: ", data)
+            },
+            cancelHandler: function () {
+                console.log("close")
+            }
+        },
+        components: {
+            DateTimePicker
+        },
+        computed: {
+            resultString: function () {
+                return JSON.stringify(this.dateTimePickerValue, null, 4)
+            }
+        },
+        data: function () {
+            return {
+                startDate: new Date(),
+                endDate: new Date(),
+                singleDate: false,
+                dateTimePickerValue: "",
+                sourcecode: `
+        <TimePicker v-model="timePickerValue" format="hh:mm:A" />
 
-data: function(){
-  return {timePickerValue: ""}
-}
+        data: function(){
+          return {timePickerValue: ""}
+        }
         `
-    };
-  }
-};
+            }
+        },
+        mounted() {
+
+        }
+    }
 </script>
 
 <style lang="scss" scoped>
-.timepickerWrapper {
-  width: 100%;
-  max-width: 600px;
-  margin: auto;
-  margin-top: 300px;
+    .timepickerWrapper {
+        width: 100%;
+        max-width: 600px;
+        margin: auto;
+        margin-top: 300px;
 
-  .demoSection {
-    width: 200px;
-    margin: auto;
-  }
+        .demoSection {
+            width: 200px;
+            margin: auto;
+        }
 
-  .resultSection {
-    margin-top: 20px;
-    text-align: left;
-  }
-  .sampleSection {
-    margin-top: 20px;
-    text-align: left;
+        .resultSection {
+            margin-top: 20px;
+            text-align: left;
+        }
 
-    .exampleWrapper {
-      padding: 20px;
-      background: #eeeeee;
+        .sampleSection {
+            margin-top: 20px;
+            text-align: left;
+
+            .exampleWrapper {
+                padding: 20px;
+                background: #eeeeee;
+            }
+        }
+
+        h2 {
+            line-height: 2.5;
+        }
     }
-  }
-
-  h2 {
-    line-height: 2.5;
-  }
-}
 </style>
