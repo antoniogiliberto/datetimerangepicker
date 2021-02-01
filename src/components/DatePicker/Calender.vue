e153415e3b6da8d9827b212e54502434a20c6ebb
Antonio Giliberto <antonio.giliberto@ngpems.com.mt>
2021-02-01 10:56:00 +0100

    fixed heatmap

--- src/components/DatePicker/Calender.vue
+++ src/components/DatePicker/Calender.vue
@@ -151,7 +151,7 @@ export default {
                 const dt = new Date(year, month, day).getTime()
 
                 // console.log(dt, this.heatMapData.startTime)
-                const i = Math.floor((dt - this.heatMapData.startTime) / 86400e3)
+                const i = Math.floor((dt - this.heatMapData.startTime) / 86400e3) + 1
                 if(dt < Date.now() && this.heatMapData.dailyData[i]){
                     // console.log(this.year, month, day, this.heatMapData)
                     const value = this.heatMapData.dailyData[i] // this.dailyDataMax
